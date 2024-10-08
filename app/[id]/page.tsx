8f5f1a765b12869bf57decc7f596ee8dcc6408a9
Mamdouh <mamdouh.mohammed919@gmail.com>
2024-10-08 13:53:19 +0300

    updates

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,11 +2,115 @@ import { CustomLink } from '@/components/layouts/CustomLink'
 import PhoneStyle from '@/components/layouts/PhoneStyle'
 import LinksContent from '@/components/sections/LinksContent'
 import { Container, Stack } from '@mui/material'
+import { Metadata } from 'next'
 import React from 'react'
 
-const Page = ({ params: { id } }: { params: { id: string } }) => {
-    console.log(id)
+const titles = {
+    "aspagh": "اصباغ",
+    "sabaagh_rakhis": "صباغ رخيص",
+    "sabaagh_shatir": "صباغ شاطر",
+    "muealim_sabagh": "معلم صباغ",
+    "sabaagh_alkuayt": "صباغ الكويت",
+    "aisbgh_alkuayt": "اصباغ الكويت",
+    "sabaagh_shatir_bi_alkuayt": "صباغ شاطر بالكويت",
+    "muealim_sabaagh_bi_alkuayt": "معلم صباغ في الكويت",
+    "faniy_sabagh": "فني صباغ الكويت",
+    "sabaagh_mumtaz_bi_alkuayt": "صباغ ممتاز بالكويت",
+    "dikurat_sabagh_lilkuayt": "صباغ ديكورات بالكويت",
+    "manazil_sabaagh_bi_alkuayt": "صباغ منازل بالكويت",
+    "shaqaq_sabaagh_bi_alkuayt": "صباغ شقق بالكويت",
+    "sabaagh_buyut_aitfal": "صباغ بيوت اطفال",
+    "sabaagh_rusumat_aitfal": "صباغ رسومات اطفال",
+    "sabaagh_rakhisat_bi_alkuayt": "صباغ رخيص بالكويت",
+    "dihanat_alkuayt": "دهانات الكويت",
+    "tarkib_waraq_judran": "تركيب ورق جدران",
+    "asbagh_abwab_khashabia": "اصباغ ابواب خشب",
+    "muealim_jabs_burd": "معلم جبس بورد",
+    "sabaagh-alsaalimia": "صباغ السالمية",
+    "sabaagh-sabah-alsaalim": "صباغ صباح السالم",
+    "sabaagh-hawalli": "صباغ حولي",
+    "sabaagh-jaber-alahmad": "صباغ جابر الاحمد",
+    "sabaagh-khaitan": "صباغ خيطان",
+    "sabaagh-alfarwaniyah": "صباغ الفروانية",
+    "sabaagh-aljabriya": "صباغ الجابرية",
+    "sabaagh-alshaab": "صباغ الشعب",
+    "sabaagh-bayan": "صباغ بيان",
+    "sabaagh-janoub-alsura": "صباغ جنوب السرة",
+    "sabaagh-mubarak": "صباغ مبارك",
+    "sabaagh-mishref": "صباغ مشرف",
+    "sabaagh-alzahraa": "صباغ الزهراء",
+    "sabaagh-alsiddiq": "صباغ الصديق",
+    "sabaagh-hateen": "صباغ حطين",
+    "sabaagh-salwa": "صباغ سلوي",
+    "sabaagh-alshuhadaa": "صباغ الشهداء",
+    "sabaagh-al-salam": "صباغ السلام",
+    "sabaagh-al-masila": "صباغ المسلية",
+    "sabaagh-almasayel": "صباغ المسايل",
+    "sabaagh-abu-alhasania": "صباغ ابو الحصاني",
+    "sabaagh-subhan": "صباغ صبحان",
+    "sabaagh-abu-ftaira": "صباغ ابو فطيرة",
+    "sabaagh-al-fintas": "صباغ الفينطيس",
+    "sabaagh-mubarak-al-kabeer": "صباغ مبارك الكبير",
+    "sabaagh-alaqeela": "صباغ العقيلة",
+    "sabaagh-alfntas": "صباغ الفنطاس",
+    "sabaagh-alraqa": "صباغ الرقة",
+    "sabaagh-abu-halifa": "صباغ ابو حليفة",
+    "sabaagh-alsabahiya": "صباغ الصباحية",
+    "sabaagh-almanqaf": "صباغ المنقف",
+    "sabaagh-almahboula": "صباغ المهبولة",
+    "sabaagh-al-ahmadi": "صباغ الاحمدي",
+    "sabaagh-al-khiran": "صباغ الخيران",
+    "sabaagh-alfhahil": "صباغ الفحيحيل",
+    "sabaagh-alwafra": "صباغ الوفرة",
+    "sabaagh-jaber-alali": "صباغ جابر العلي",
+    "sabaagh-fahad-alahmad": "صباغ فهد الاحمد",
+    "sabaagh-aldaiya": "صباغ الدعية",
+    "sabaagh-aldasma": "صباغ الدسمة",
+    "sabaagh-aldohah": "صباغ الدوحة",
+    "sabaagh-alkhaldiya": "صباغ الخالدية",
+    "sabaagh-alrawdah": "صباغ الروضة",
+    "sabaagh-alrai": "صباغ الري",
+    "sabaagh-alshuwaykh": "صباغ الشويخ",
+    "sabaagh-asharq": "صباغ الشرق",
+    "sabaagh-alshamiya": "صباغ الشامية",
+    "sabaagh-alsurra": "صباغ السرة",
+    "sabaagh-alqadesiya": "صباغ القادسية",
+    "sabaagh-alqurawan": "صباغ القروان",
+    "sabaagh-alfaihaa": "صباغ الفيحاء",
+    "sabaagh-aladiliya": "صباغ العديلية",
+    "sabaagh-alsulaybikhat": "صباغ الصليبخات",
+    "sabaagh-kaifan": "صباغ كيفان",
+    "sabaagh-qurtoba": "صباغ قرطبة",
+    "sabaagh-ghranata": "صباغ غرناطة",
+    "sabaagh-abdullah-al-salim": "صباغ ضاحية عبدالله السالم",
+    "sabaagh-dasman": "صباغ دسمان",
+    "sabaagh-bneid-alqar": "صباغ بنيد القار",
+    "sabaagh-alnahda": "صباغ النهضة",
+    "sabaagh-alnuzha": "صباغ النزهة",
+    "sabaagh-almansouriya": "صباغ المنصورية",
+    "sabaagh-mantiqa-al-ashira": "صباغ المنطقة العاشرة",
+    "sabaagh-alandalus": "صباغ الاندلس",
+    "sabaagh-alqrean": "صباغ القرين",
+    "sabaagh-alkswor": "صباغ القصور",
+    "sabaagh-aladaan": "صباغ العدان",
+}
 
+type Props = {
+    params: { id: string };
+};
+
+export const generateMetadata = async ({
+    params,
+}: Props): Promise<Metadata> => {
+    const title = titles[params.id as keyof typeof titles];
+
+    return {
+        title: `صباغ الكويت 51108895 - صباغ شاطر ورخيص - ${title}`,
+        keywords: `صباغ الكويت, صباغ شاطر, صباغ رخيص, اصباغ داخلية وخارجية, صباغ في الكويت, اصباغ حديثة, صباغ الكويت بأفضل الأسعار, ${title}`,
+    };
+}
+
+const Page = ({ params: { id } }: { params: { id: string } }) => {
     return (
         <Container maxWidth="lg">
             <Stack my={5} spacing={3} alignItems={"center"}>
--- components/sections/Links.tsx
+++ components/sections/Links.tsx
@@ -8,72 +8,72 @@ const links = [
     {
         id: 1,
         title: "اصباغ",
-        href: "/aspagh",
+        href: "aspagh",
     },
     {
         id: 2,
         title: "صباغ رخيص",
-        href: "/sabaagh-rakhis",
+        href: "sabaagh-rakhis",
     },
     {
         id: 3,
         title: "صباغ شاطر",
-        href: "/sabaagh-shatir",
+        href: "sabaagh-shatir",
     },
     {
         id: 4,
         title: "معلم صباغ",
-        href: "/muealim-sabagh",
+        href: "muealim-sabagh",
     },
     {
         id: 5,
         title: "صباغ الكويت",
-        href: "/sabaagh-alkuayt",
+        href: "sabaagh-alkuayt",
     },
     {
         id: 6,
         title: "اصباغ الكويت",
-        href: "/aisbgh-alkuayt",
+        href: "aisbgh-alkuayt",
     },
     {
         id: 7,
         title: "صباغ شاطر بالكويت",
-        href: "/sabaagh-shatir-bi-alkuayt",
+        href: "sabaagh-shatir-bi-alkuayt",
     },
     {
         id: 8,
         title: "معلم صباغ في الكويت",
-        href: "/muealim-sabaagh-bi-alkuayt",
+        href: "muealim-sabaagh-bi-alkuayt",
     },
     {
         id: 9,
         title: "فني صباغ الكويت",
-        href: "/faniy-sabagh",
+        href: "faniy-sabagh",
     },
     {
         id: 10,
         title: "صباغ ممتاز بالكويت",
-        href: "/sabaagh-mumtaz-bi-alkuayt",
+        href: "sabaagh-mumtaz-bi-alkuayt",
     },
     {
         id: 11,
         title: "صباغ ديكورات بالكويت",
-        href: "/dikurat-sabagh-lilkuayt",
+        href: "dikurat-sabagh-lilkuayt",
     },
     {
         id: 12,
         title: "صباغ منازل بالكويت",
-        href: "/manazil-sabaagh-bi-alkuayt",
+        href: "manazil-sabaagh-bi-alkuayt",
     },
     {
         id: 13,
         title: "صباغ شقق بالكويت",
-        href: "/shaqaq-sabaagh-bi-alkuayt",
+        href: "shaqaq-sabaagh-bi-alkuayt",
     },
     {
         id: 14,
         title: "صباغ بيوت اطفال",
-        href: "/sabaagh-buyut-aitfal",
+        href: "sabaagh-buyut-aitfal",
     },
     {
         id: 15,
@@ -109,73 +109,341 @@ const links = [
 ]
 
 export const zones = [
-    { id: 21, title: "صباغ السالمية", href: "sabaagh-alsaalimia" },
-    { id: 22, title: "صباغ صباح السالم", href: "sabaagh-sabah-alsaalim" },
-    { id: 23, title: "صباغ حولي", href: "sabaagh-hawalli" },
-    { id: 24, title: "صباغ جابر الاحمد", href: "sabaagh-jaber-alahmad" },
-    { id: 25, title: "صباغ خيطان", href: "sabaagh-khaitan" },
-    { id: 26, title: "صباغ الفروانية", href: "sabaagh-alfarwaniyah" },
-    { id: 27, title: "صباغ الجابرية", href: "sabaagh-aljabriya" },
-    { id: 28, title: "صباغ الشعب", href: "sabaagh-alshaab" },
-    { id: 29, title: "صباغ بيان", href: "sabaagh-bayan" },
-    { id: 30, title: "صباغ جنوب السرة", href: "sabaagh-janoub-alsura" },
-    { id: 31, title: "صباغ مبارك", href: "sabaagh-mubarak" },
-    { id: 32, title: "صباغ مشرف", href: "sabaagh-mishref" },
-    { id: 33, title: "صباغ الزهراء", href: "sabaagh-alzahraa" },
-    { id: 34, title: "صباغ الصديق", href: "sabaagh-alsiddiq" },
-    { id: 35, title: "صباغ حطين", href: "sabaagh-hateen" },
-    { id: 36, title: "صباغ سلوي", href: "sabaagh-salwa" },
-    { id: 37, title: "صباغ الشهداء", href: "sabaagh-alshuhadaa" },
-    { id: 38, title: "صباغ السلام", href: "sabaagh-al-salam" },
-    { id: 39, title: "صباغ المسلية", href: "sabaagh-al-masila" },
-    { id: 40, title: "صباغ المسايل", href: "sabaagh-almasayel" },
-    { id: 41, title: "صباغ ابو الحصاني", href: "sabaagh-abu-alhasania" },
-    { id: 42, title: "صباغ صبحان", href: "sabaagh-subhan" },
-    { id: 43, title: "صباغ ابو فطيرة", href: "sabaagh-abu-ftaira" },
-    { id: 44, title: "صباغ الفينطيس", href: "sabaagh-al-fintas" },
-    { id: 45, title: "صباغ مبارك الكبير", href: "sabaagh-mubarak-al-kabeer" },
-    { id: 46, title: "صباغ العقيلة", href: "sabaagh-alaqeela" },
-    { id: 47, title: "صباغ الفنطاس", href: "sabaagh-alfntas" },
-    { id: 48, title: "صباغ الرقة", href: "sabaagh-alraqa" },
-    { id: 49, title: "صباغ ابو حليفة", href: "sabaagh-abu-halifa" },
-    { id: 50, title: "صباغ الصباحية", href: "sabaagh-alsabahiya" },
-    { id: 51, title: "صباغ المنقف", href: "sabaagh-almanqaf" },
-    { id: 52, title: "صباغ المهبولة", href: "sabaagh-almahboula" },
-    { id: 53, title: "صباغ الاحمدي", href: "sabaagh-al-ahmadi" },
-    { id: 54, title: "صباغ الخيران", href: "sabaagh-al-khiran" },
-    { id: 55, title: "صباغ الفحيحيل", href: "sabaagh-alfhahil" },
-    { id: 56, title: "صباغ الوفرة", href: "sabaagh-alwafra" },
-    { id: 57, title: "صباغ جابر العلي", href: "sabaagh-jaber-alali" },
-    { id: 58, title: "صباغ فهد الاحمد", href: "sabaagh-fahad-alahmad" },
-    { id: 59, title: "صباغ الدعية", href: "sabaagh-aldaiya" },
-    { id: 60, title: "صباغ الدسمة", href: "sabaagh-aldasma" },
-    { id: 61, title: "صباغ الدوحة", href: "sabaagh-aldohah" },
-    { id: 62, title: "صباغ الخالدية", href: "sabaagh-alkhaldiya" },
-    { id: 63, title: "صباغ الروضة", href: "sabaagh-alrawdah" },
-    { id: 64, title: "صباغ الري", href: "sabaagh-alrai" },
-    { id: 65, title: "صباغ الشويخ", href: "sabaagh-alshuwaykh" },
-    { id: 66, title: "صباغ الشرق", href: "sabaagh-asharq" },
-    { id: 67, title: "صباغ الشامية", href: "sabaagh-alshamiya" },
-    { id: 68, title: "صباغ السرة", href: "sabaagh-alsurra" },
-    { id: 69, title: "صباغ القادسية", href: "sabaagh-alqadesiya" },
-    { id: 70, title: "صباغ القروان", href: "sabaagh-alqurawan" },
-    { id: 71, title: "صباغ الفيحاء", href: "sabaagh-alfaihaa" },
-    { id: 72, title: "صباغ العديلية", href: "sabaagh-aladiliya" },
-    { id: 73, title: "صباغ الصليبخات", href: "sabaagh-alsulaybikhat" },
-    { id: 74, title: "صباغ كيفان", href: "sabaagh-kaifan" },
-    { id: 75, title: "صباغ قرطبة", href: "sabaagh-qurtoba" },
-    { id: 76, title: "صباغ غرناطة", href: "sabaagh-ghranata" },
-    { id: 77, title: "صباغ ضاحية عبدالله السالم", href: "sabaagh-abdullah-al-salim" },
-    { id: 78, title: "صباغ دسمان", href: "sabaagh-dasman" },
-    { id: 79, title: "صباغ بنيد القار", href: "sabaagh-bneid-alqar" },
-    { id: 80, title: "صباغ النهضة", href: "sabaagh-alnahda" },
-    { id: 81, title: "صباغ النزهة", href: "sabaagh-alnuzha" },
-    { id: 82, title: "صباغ المنصورية", href: "sabaagh-almansouriya" },
-    { id: 83, title: "صباغ المنطقة العاشرة", href: "sabaagh-mantiqa-al-ashira" },
-    { id: 84, title: "صباغ الاندلس", href: "sabaagh-alandalus" },
-    { id: 85, title: "صباغ القرين", href: "sabaagh-alqrean" },
-    { id: 86, title: "صباغ القصور", href: "sabaagh-alkswor" },
-    { id: 87, title: "صباغ العدان", href: "sabaagh-aladaan" }
+    {
+        id: 21,
+        title: "صباغ السالمية",
+        href: "sabaagh-alsaalimia"
+    },
+    {
+        id: 22,
+        title: "صباغ صباح السالم",
+        href: "sabaagh-sabah-alsaalim"
+    },
+    {
+        id: 23,
+        title: "صباغ حولي",
+        href: "sabaagh-hawalli"
+    },
+    {
+        id: 24,
+        title: "صباغ جابر الاحمد",
+        href: "sabaagh-jaber-alahmad"
+    },
+    {
+        id: 25,
+        title: "صباغ خيطان",
+        href: "sabaagh-khaitan"
+    },
+    {
+        id: 26,
+        title: "صباغ الفروانية",
+        href: "sabaagh-alfarwaniyah"
+    },
+    {
+        id: 27,
+        title: "صباغ الجابرية",
+        href: "sabaagh-aljabriya"
+    },
+    {
+        id: 28,
+        title: "صباغ الشعب",
+        href: "sabaagh-alshaab"
+    },
+    {
+        id: 29,
+        title: "صباغ بيان",
+        href: "sabaagh-bayan"
+    },
+    {
+        id: 30,
+        title: "صباغ جنوب السرة",
+        href: "sabaagh-janoub-alsura"
+    },
+    {
+        id: 31,
+        title: "صباغ مبارك",
+        href: "sabaagh-mubarak"
+    },
+    {
+        id: 32,
+        title: "صباغ مشرف",
+        href: "sabaagh-mishref"
+    },
+    {
+        id: 33,
+        title: "صباغ الزهراء",
+        href: "sabaagh-alzahraa"
+    },
+    {
+        id: 34,
+        title: "صباغ الصديق",
+        href: "sabaagh-alsiddiq"
+    },
+    {
+        id: 35,
+        title: "صباغ حطين",
+        href: "sabaagh-hateen"
+    },
+    {
+        id: 36,
+        title: "صباغ سلوي",
+        href: "sabaagh-salwa"
+    },
+    {
+        id: 37,
+        title: "صباغ الشهداء",
+        href: "sabaagh-alshuhadaa"
+    },
+    {
+        id: 38,
+        title: "صباغ السلام",
+        href: "sabaagh-al-salam"
+    },
+    {
+        id: 39,
+        title: "صباغ المسلية",
+        href: "sabaagh-al-masila"
+    },
+    {
+        id: 40,
+        title: "صباغ المسايل",
+        href: "sabaagh-almasayel"
+    },
+    {
+        id: 41,
+        title: "صباغ ابو الحصاني",
+        href: "sabaagh-abu-alhasania"
+    },
+    {
+        id: 42,
+        title: "صباغ صبحان",
+        href: "sabaagh-subhan"
+    },
+    {
+        id: 43,
+        title: "صباغ ابو فطيرة",
+        href: "sabaagh-abu-ftaira"
+    },
+    {
+        id: 44,
+        title: "صباغ الفينطيس",
+        href: "sabaagh-al-fintas"
+    },
+    {
+        id: 45,
+        title: "صباغ مبارك الكبير",
+        href: "sabaagh-mubarak-al-kabeer"
+    },
+    {
+        id: 46,
+        title: "صباغ العقيلة",
+        href: "sabaagh-alaqeela"
+    },
+    {
+        id: 47,
+        title: "صباغ الفنطاس",
+        href: "sabaagh-alfntas"
+    },
+    {
+        id: 48,
+        title: "صباغ الرقة",
+        href: "sabaagh-alraqa"
+    },
+    {
+        id: 49,
+        title: "صباغ ابو حليفة",
+        href: "sabaagh-abu-halifa"
+    },
+    {
+        id: 50,
+        title: "صباغ الصباحية",
+        href: "sabaagh-alsabahiya"
+    },
+    {
+        id: 51,
+        title: "صباغ المنقف",
+        href: "sabaagh-almanqaf"
+    },
+    {
+        id: 52,
+        title: "صباغ المهبولة",
+        href: "sabaagh-almahboula"
+    },
+    {
+        id: 53,
+        title: "صباغ الاحمدي",
+        href: "sabaagh-al-ahmadi"
+    },
+    {
+        id: 54,
+        title: "صباغ الخيران",
+        href: "sabaagh-al-khiran"
+    },
+    {
+        id: 55,
+        title: "صباغ الفحيحيل",
+        href: "sabaagh-alfhahil"
+    },
+    {
+        id: 56,
+        title: "صباغ الوفرة",
+        href: "sabaagh-alwafra"
+    },
+    {
+        id: 57,
+        title: "صباغ جابر العلي",
+        href: "sabaagh-jaber-alali"
+    },
+    {
+        id: 58,
+        title: "صباغ فهد الاحمد",
+        href: "sabaagh-fahad-alahmad"
+    },
+    {
+        id: 59,
+        title: "صباغ الدعية",
+        href: "sabaagh-aldaiya"
+    },
+    {
+        id: 60,
+        title: "صباغ الدسمة",
+        href: "sabaagh-aldasma"
+    },
+    {
+        id: 61,
+        title: "صباغ الدوحة",
+        href: "sabaagh-aldohah"
+    },
+    {
+        id: 62,
+        title: "صباغ الخالدية",
+        href: "sabaagh-alkhaldiya"
+    },
+    {
+        id: 63,
+        title: "صباغ الروضة",
+        href: "sabaagh-alrawdah"
+    },
+    {
+        id: 64,
+        title: "صباغ الري",
+        href: "sabaagh-alrai"
+    },
+    {
+        id: 65,
+        title: "صباغ الشويخ",
+        href: "sabaagh-alshuwaykh"
+    },
+    {
+        id: 66,
+        title: "صباغ الشرق",
+        href: "sabaagh-asharq"
+    },
+    {
+        id: 67,
+        title: "صباغ الشامية",
+        href: "sabaagh-alshamiya"
+    },
+    {
+        id: 68,
+        title: "صباغ السرة",
+        href: "sabaagh-alsurra"
+    },
+    {
+        id: 69,
+        title: "صباغ القادسية",
+        href: "sabaagh-alqadesiya"
+    },
+    {
+        id: 70,
+        title: "صباغ القروان",
+        href: "sabaagh-alqurawan"
+    },
+    {
+        id: 71,
+        title: "صباغ الفيحاء",
+        href: "sabaagh-alfaihaa"
+    },
+    {
+        id: 72,
+        title: "صباغ العديلية",
+        href: "sabaagh-aladiliya"
+    },
+    {
+        id: 73,
+        title: "صباغ الصليبخات",
+        href: "sabaagh-alsulaybikhat"
+    },
+    {
+        id: 74,
+        title: "صباغ كيفان",
+        href: "sabaagh-kaifan"
+    },
+    {
+        id: 75,
+        title: "صباغ قرطبة",
+        href: "sabaagh-qurtoba"
+    },
+    {
+        id: 76,
+        title: "صباغ غرناطة",
+        href: "sabaagh-ghranata"
+    },
+    {
+        id: 77,
+        title: "صباغ ضاحية عبدالله السالم",
+        href: "sabaagh-abdullah-al-salim"
+    },
+    {
+        id: 78,
+        title: "صباغ دسمان",
+        href: "sabaagh-dasman"
+    },
+    {
+        id: 79,
+        title: "صباغ بنيد القار",
+        href: "sabaagh-bneid-alqar"
+    },
+    {
+        id: 80,
+        title: "صباغ النهضة",
+        href: "sabaagh-alnahda"
+    },
+    {
+        id: 81,
+        title: "صباغ النزهة",
+        href: "sabaagh-alnuzha"
+    },
+    {
+        id: 82,
+        title: "صباغ المنصورية",
+        href: "sabaagh-almansouriya"
+    },
+    {
+        id: 83,
+        title: "صباغ المنطقة العاشرة",
+        href: "sabaagh-mantiqa-al-ashira"
+    },
+    {
+        id: 84,
+        title: "صباغ الاندلس",
+        href: "sabaagh-alandalus"
+    },
+    {
+        id: 85,
+        title: "صباغ القرين",
+        href: "sabaagh-alqrean"
+    },
+    {
+        id: 86,
+        title: "صباغ القصور",
+        href: "sabaagh-alkswor"
+    },
+    {
+        id: 87,
+        title: "صباغ العدان",
+        href: "sabaagh-aladaan"
+    }
 ];
 
 const allLinks = links.concat(zones)
@@ -183,7 +451,7 @@ const allLinks = links.concat(zones)
 const Links = () => {
     return (
         <Container maxWidth="lg" sx={{ my: 5 }} id="articles">
-            <SectionTitle sectionTitle={"صباغ الكويت"} variant='body1'/>
+            <SectionTitle sectionTitle={"صباغ الكويت"} variant='body1' />
             <Stack mt={4} direction={"row"} alignItems={"center"} flexWrap={"wrap"} justifyContent={"center"} useFlexGap={true} spacing={2}>
                 {allLinks.map(element => (
                     <CustomLink title={element.title} href={element.href} key={element.id}>
